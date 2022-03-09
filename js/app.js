01899ddfa26be8be8e8df98c1a2b5ae1d0a73fd3
Ayon <ayonjodder177@gmail.com>
2022-03-10 00:03:56 +0600

    Product images fix

--- js/app.js
+++ js/app.js
@@ -7,9 +7,8 @@ const loadProducts = () => {
 
 // show all product in UI 
 const showProducts = (products) => {
-  const allProducts = products.map((pd) => pd);
-  for (const product of allProducts) {
-    const image = product.images;
+  for (const product of products) {
+    const image = product.image;
     const div = document.createElement("div");
     div.classList.add("product");
     div.innerHTML = `<div class="single-product">
